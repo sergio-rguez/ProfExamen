6a9ab714ae03f034fcfe1c6273e9cfd975c926b6
Sergio Rodríguez <sergiorguez97@hotmail.com>
2020-10-15 13:32:16 +0200

    Cambios

--- ExamenProf/src/main/java/luispalomo/ExamenProf/App.java
+++ ExamenProf/src/main/java/luispalomo/ExamenProf/App.java
@@ -10,13 +10,17 @@ public class App
 	private int notaExamen (int a, int b) {
 		int result = 0;
 		
+		// nodo 1
 		while (a>b) {
 			a--;
 		}
 		
+		// nodo 2
 		if(a<=b) {
+			// nodo 3
 			result = b;
 		}else {
+			// nodo 4
 			b = 0;
 		}
 		
